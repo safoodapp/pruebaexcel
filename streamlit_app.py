57a434c1627a413a6adad2f54c6b9ff62dee33a4
safoodapp <consultoria@safood.es>
2025-12-04 10:34:14 +0100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,14 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Prueba de lectura desde Google Drive (Google Sheets)")
+st.title("PRUEBA CON LA HOJA REAL")
 
-url = "https://docs.google.com/spreadsheets/d/1zbihxqDHWXHm1CKVuFH6pm8ADKyhaL5lTOLgfz8YUJ0/export?format=csv&gid=0"
+url = "https://docs.google.com/spreadsheets/d/1M-1zM8pxosv75N5gCtWaPkE1beQBOaMD/export?format=csv&gid=707739207"
 
 try:
     df = pd.read_csv(url)
-    st.success("Datos cargados correctamente")
+    st.success("Hoja REAL cargada correctamente")
     st.dataframe(df)
 except Exception as e:
-    st.error("Error al cargar la hoja")
+    st.error("Error cargando la hoja REAL")
     st.write(e)
