1757dfae858d0f58fac5eb9b97b620062152a8b7
safoodapp <consultoria@safood.es>
2025-12-04 10:26:58 +0100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.title("Prueba de lectura desde Google Drive (Google Sheets)")
 
-url = "AQUI_PONDRÁS_TU_ENLACE_CSV"
+url = "https://docs.google.com/spreadsheets/d/1zbihxqDHWXHm1CKVuFH6pm8ADKyhaL5lTOLgfz8YUj0/export?format=csv&gid=0"
 
 try:
     df = pd.read_csv(url)
